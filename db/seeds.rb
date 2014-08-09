8c0ad49fd8ab57340211e386d92a52681b42b27a
Bryce Matsuda <iexist4jcore@gmail.com>
2014-08-08 19:53:51 -1000

    Plant some seeds, fix some layout issues, implement video embedding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 admin = Admin.create(:email => 'admin@example.com', :password => 'password')
-show = Show.create(:name => 'WinDEU Hates You', :visible => 'true')
-season = Season.create(:name => 'I Ran Out of Titles', :visible => 'true', :show_id => 1, :position => 1)
-episode = Episode.create(:season_id => 1, :name => 'Stage 1', :position => 1, :visible => 'true', :content_type => 'text', :content => 'https://www.youtube.com/watch?v=QjdBvPWUHiQ', :description => 'Song: Space Diver Tama')
+
+windeu = Show.create(:name => 'WinDEU Hates You', :visible => 'true')
+iroot = Season.create(:name => 'I Ran Out of Titles', :visible => 'true', :show_id => 1, :position => 1, :permalink => 'iroot')
+lovesu = Season.create(:name =>'Loves You: The Redemption', :visible => 'true', :show_id => 1, :position => 2, :permalink => 'lovesu')
+jkitneverends = Season.create(:name =>'Just Kidding, It Never Ends', :visible => 'true', :show_id => 1, :position => 3, :permalink => 'jkitneverends')
+
+iroot1 = Episode.create(:season_id => 1, :name => 'Stage 1', :position => 1, :visible => 'true', :content_type => 'text', :content => '<iframe width="560" height="315" src="//www.youtube.com/embed/QjdBvPWUHiQ?rel=0" frameborder="0" allowfullscreen></iframe>', :description => 'Song: Space Diver Tama')
+loves2 = Episode.create(:season_id => 2, :name => 'Stage 2', :position => 2, :visible => 'true', :content_type => 'text', :content => '<iframe width="560" height="315" src="//www.youtube.com/embed/CUXMpAMlSy0?rel=0" frameborder="0" allowfullscreen></iframe>', :description => 'Song: t+pazolite - nekosogi Destruction')
+jkitneverends3 = Episode.create(:season_id => 3, :name => 'Stage 3 - Mechanical Maze', :position => 3, :visible => 'true', :content_type => 'text', :content => '<iframe width="560" height="315" src="//www.youtube.com/embed/yBF2amXpTks?rel=0" frameborder="0" allowfullscreen></iframe>', :description => 'Song: Makkon - The Little Toy Shop')
